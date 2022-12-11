8300b4e96392056824085afbf377c046c4f1adce
ksen <ksen-burl@yandex.ru>
2022-12-11 13:03:40 +0500

    First commit for code

---
+++ main.py
@@ -0,0 +1,42 @@
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
+from PyQt5.QtCore import Qt
+from PyQt5 import uic
+from random import randint 
+
+
+class Test(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
+        self.pushButton.setFixedSize(80, 80)
+        self.pushButton.clicked.connect(self.circle)
+
+        self.label = QLabel()  
+        canvas = QPixmap(400, 400)
+        self.label.setPixmap(canvas)
+
+        layout = QGridLayout(self.centralwidget)
+        layout.addWidget(self.pushButton, 0, 0, alignment=Qt.AlignCenter)
+        layout.addWidget(self.label, 1, 0)
+
+    def circle(self):
+        x, y = [randint(10, 350) for i in range(2)]
+        h = randint(10, 200)
+        painter = QPainter(self.label.pixmap())
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor(255, 255, 0))
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, h, h)
+        painter.end()
+        self.update()
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    w = Test()
+    w.show()
+    sys.exit(app.exec_())
